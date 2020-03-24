f09dcc34be3a3dcf76e97682b83b09962cd398ff
AndreeaMagdici2020 <31406433+AndreeaMagdici2020@users.noreply.github.com>
2020-03-24 22:22:47 +0200

    Add initial files

---
+++ IntwTrack.js
@@ -0,0 +1,56 @@
+class createElem{
+    constructor(type, parent)
+    {
+        this.type=type;
+        this.parent=parent;
+    }
+
+    render()
+    {
+        this.element=document.createElement(this.type);
+        this.parent.appendChild(this.element);
+        
+    }
+
+    design(height,width)
+    {
+        this.element.style.height=height;
+        this.element.style.width=width;
+        this.element.style.border="solid";
+        this.element.style.borderColor="black";
+        this.element.style.margin=("0 auto");
+    }
+    picture_design(src, height, width)
+    {
+        this.element.style.height=height;
+        this.element.style.width=width;
+        this.element.style.margin=("0 auto");
+        this.element.style.borderRadius="50px";
+        this.element.src=src;
+    }
+};
+
+var header=new createElem("div", document.body);
+header.render();
+header.design("50px", "1000px");
+
+var main=new createElem("div", document.body);
+main.render();
+main.design("500px", "1000px");
+
+
+
+var main_pic=new createElem("img",main.element);
+main_pic.render();
+main_pic.design("50px", "100px");
+main_pic.picture_design("0_nIqyCjF2MbjUi-Jf.jpg", "100px", "100px");
+
+
+
+
+
+
+
+
+
+
